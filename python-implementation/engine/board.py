526d3c20bf5043454de594012be1b0825f1f6fd5
Vaishak Menon <vaishakkmenon@gmail.com>
2025-05-16 21:57:27 +0000

    board display and set up is complete

--- python-implementation/engine/board.py
+++ python-implementation/engine/board.py
@@ -27,12 +27,6 @@ class Board:
             [self.EMPTY for _ in range(self.FILES)] for _ in range(self.RANKS)
         ]
 
-    def __repr__(self):
-        for i in range(self.RANKS):
-            for j in range(self.FILES):
-                print(f"{self.squares[i][j]} ", end="")
-            print()
-
     def __str__(self) -> str:
         rows = []
         reverse = reversed(self.squares)
@@ -41,8 +35,32 @@ class Board:
         rows.append("  a b c d e f g h")
         return "\n".join(rows)
 
+    def __getitem__(self, pos: tuple[int, int]) -> str:
+        file, rank = pos
+        return self.squares[rank - 1][file - 1]
+
+    def __setitem__(self, pos: tuple[int, int], piece: str):
+        file, rank = pos
+        self.squares[rank - 1][file - 1] = piece
+
+    def init_positions(self):
+        for i in range(self.RANKS):
+            for j in range(self.FILES):
+                self.squares[i][j] = self.EMPTY
+
+        for f in range(self.FILES):
+            self.squares[1][f] = self.PIECES["WP"]
+            self.squares[6][f] = self.PIECES["BP"]
+
+        WHITEPIECES = ["WR", "WN", "WB", "WQ", "WK", "WB", "WN", "WR"]
+        BLACKPIECES = ["BR", "BN", "BB", "BQ", "BK", "BB", "BN", "BR"]
+        for f in range(self.FILES):
+            self.squares[0][f] = self.PIECES[WHITEPIECES[f]]
+            self.squares[7][f] = self.PIECES[BLACKPIECES[f]]
+
 
 board = Board()
-# print(len(board.squares), len(board.squares[0]))  # should be 8, 8
-# board.__repr__()
+print(board)
+board.init_positions()
+print()
 print(board)
