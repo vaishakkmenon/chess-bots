9234b2b2f271c0ea6b2f12c7aac992db7ead580c
Vaishak Menon <vaishakkmenon@gmail.com>
2025-05-21 20:45:59 +0000

    updated queen moves for stylistic syntax and structure changes

--- python-implementation/engine/moves/queen.py
+++ python-implementation/engine/moves/queen.py
@@ -1,14 +1,15 @@
 from board import Board
+from typing import List
 from offsets import QUEEN_OFFSETS
+
+from .move import Move
 from .helpers import check_bounds
 
 
-def queen_moves(
-    board: Board, color: str
-) -> list[tuple[tuple[int, int], tuple[int, int], str | None]]:
+def queen_moves(board: Board, color: str) -> List[Move]:
     """
     Generate all queen slides for the given color.
-    Returns a list of (from_sq, to_sq, None) triples.
+    Returns a list of Move objects.
     """
     moves = []
 
@@ -19,23 +20,24 @@ def queen_moves(
 
     for file in range(1, 9):
         for rank in range(1, 9):
-            if board[file, rank] == queen_char:
-                for moveF, moveR in QUEEN_OFFSETS:
-                    targetFile = file + moveF
-                    targetRank = rank + moveR
-                    target = (targetFile, targetRank)
+            if board[file, rank] != queen_char:
+                continue
+            from_sq = (file, rank)
+            for move_f, move_r in QUEEN_OFFSETS:
+                target_file = file + move_f
+                target_rank = rank + move_r
 
-                    while check_bounds(targetFile, targetRank):
-                        target_square = board[targetFile, targetRank]
-                        if target_square == board.EMPTY:
-                            moves.append(((file, rank), target, None))
-                        else:
-                            if target_square.isupper() != queen_char.isupper():
-                                moves.append(((file, rank), target, None))
-                            break
+                while check_bounds(target_file, target_rank):
+                    to_sq = (target_file, target_rank)
+                    target_square = board[target_file, target_rank]
+                    if target_square == board.EMPTY:
+                        moves.append(Move(from_sq, to_sq))
+                    else:
+                        if target_square.isupper() != queen_char.isupper():
+                            moves.append(Move(from_sq, to_sq))
+                        break
 
-                        targetFile += moveF
-                        targetRank += moveR
-                        target = (targetFile, targetRank)
+                    target_file += move_f
+                    target_rank += move_r
 
     return moves
