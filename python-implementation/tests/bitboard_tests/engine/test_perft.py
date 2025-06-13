9d59e1cf48bc975894b85fe0c8db11a2058b215b
Vaishak Menon <vaishakkmenon@gmail.com>
2025-06-13 02:06:18 +0000

    added draw detection to perft

--- python-implementation/tests/bitboard_tests/engine/test_perft.py
+++ python-implementation/tests/bitboard_tests/engine/test_perft.py
@@ -2,7 +2,33 @@ import pytest
 
 from engine.bitboard.board import Board
 from engine.bitboard.generator import generate_legal_moves
+from engine.bitboard.constants import WHITE, WHITE_KING, BLACK_KING
 from engine.bitboard.perft import perft_count, perft_divide, perft_hashed_root
+
+
+def make_kings_only_board() -> Board:
+    b = Board()
+    b.bitboards = [0] * 12
+    b.white_occ = b.black_occ = b.all_occ = 0
+    b.ep_square = None
+    b.castling_rights = 0
+    b.side_to_move = WHITE
+    b.halfmove_clock = 0
+    b.fullmove_number = 1
+    b.square_to_piece = [None] * 64
+    b.bitboards[WHITE_KING] = 1 << 4
+    b.bitboards[BLACK_KING] = 1 << 60
+    b.update_occupancies()
+    for idx, bb in enumerate(b.bitboards):
+        t = bb
+        while t:
+            lsb = t & -t
+            sq = lsb.bit_length() - 1
+            b.square_to_piece[sq] = idx
+            t ^= lsb
+    b._compute_zobrist_from_scratch()
+    b.zobrist_history = [b.zobrist_key]
+    return b
 
 
 def test_perft_depth_zero_is_one():
@@ -47,3 +73,21 @@ def test_perft_hashed_vs_perft_count():
     b = Board()
     assert perft_count(b, 3) == perft_hashed_root(b, 3)
     assert perft_count(b, 4) == perft_hashed_root(b, 4)
+
+
+def test_perft_respects_fifty_move_draw():
+    b = Board()
+    b.halfmove_clock = 100
+    assert perft_count(b, 1, respect_draws=True) == 1
+
+
+def test_perft_respects_repetition():
+    b = Board()
+    for _ in range(2):
+        b.zobrist_history.append(b.zobrist_key)
+    assert perft_count(b, 1, respect_draws=True) == 1
+
+
+def test_perft_respects_insufficient_material():
+    b = make_kings_only_board()
+    assert perft_count(b, 1, respect_draws=True) == 1
