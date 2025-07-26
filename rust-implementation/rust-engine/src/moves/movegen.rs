2594adc4f82f9890b5ec66ea301d2cabd4013f8c
Vaishak Menon <vaishakkmenon@gmail.com>
2025-07-26 04:35:54 +0000

    basic king 1 step move generation implemented

--- rust-implementation/rust-engine/src/moves/movegen.rs
+++ rust-implementation/rust-engine/src/moves/movegen.rs
@@ -1,10 +1,9 @@
 use crate::board::{Board, Piece};
+use crate::moves::king::KING_ATTACKS;
 use crate::moves::knight::KNIGHT_ATTACKS;
 use crate::moves::magic::MagicTables;
-use crate::moves::magic::masks::bishop_vision_mask;
-use crate::moves::magic::masks::rook_vision_mask;
-use crate::moves::magic::structs::BishopMagicTables;
-use crate::moves::magic::structs::RookMagicTables;
+use crate::moves::magic::masks::{bishop_vision_mask, rook_vision_mask};
+use crate::moves::magic::structs::{BishopMagicTables, RookMagicTables};
 use crate::moves::types::Move;
 use crate::square::Square;
 
@@ -132,5 +131,37 @@ pub fn generate_queen_moves(board: &Board, tables: &MagicTables, moves: &mut Vec
                 is_castling: false,
             });
         }
+    }
+}
+
+pub fn generate_king_moves(board: &Board, moves: &mut Vec<Move>) {
+    let color = board.side_to_move;
+    let king_bb = board.pieces(Piece::King, color);
+
+    if king_bb == 0 {
+        return; // should never happen in a legal position
+    }
+
+    let from = king_bb.trailing_zeros() as u8;
+
+    let attacks = KING_ATTACKS[from as usize];
+    let friendly = board.occupancy(color);
+    let enemy = board.opponent_occupancy(color);
+
+    let targets = attacks & !friendly;
+
+    let mut targets_bb = targets;
+    while targets_bb != 0 {
+        let to = targets_bb.trailing_zeros() as u8;
+        targets_bb &= targets_bb - 1;
+
+        moves.push(Move {
+            from: Square::from_index(from),
+            to: Square::from_index(to),
+            promotion: None,
+            is_capture: (1u64 << to) & enemy != 0,
+            is_en_passant: false,
+            is_castling: false,
+        });
     }
 }
